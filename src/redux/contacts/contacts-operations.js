e2be26cf7c1fcc7c9d55025285d3e3cd84ba64d8
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-01-27 00:58:18 +0200

    Initial commit

---
+++ src/redux/contacts/contacts-operations.js
@@ -0,0 +1,31 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import {
+  fetchContacts,
+  addContact,
+  delContact,
+} from '../../servises/apiContacts';
+
+// fetchAllContacts thunk
+export const fetchAllContacts = createAsyncThunk('contact/fetch', async () => {
+  const contacts = await fetchContacts();
+  return contacts;
+});
+
+// createContact thunk
+export const createContact = createAsyncThunk('contact/create', async data => {
+  const newContact = {
+    name: data,
+    number: data,
+  };
+  const contact = await addContact(newContact);
+  return contact;
+});
+
+// deleteContact thunk
+export const deleteContact = createAsyncThunk(
+  'contact/delete',
+  async contactId => {
+    const contact = await delContact(contactId);
+    return contact;
+  },
+);
